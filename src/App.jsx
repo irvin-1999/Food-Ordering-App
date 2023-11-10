d7d0b59cda5fb9d9867dd8dc125325ae2ad39c55
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2023-11-10 11:10:30 +0200

    add the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from "./Layout/Header"
 
 
-function App() {
+const App=()=> {
 
   return (
     <>
-    <h1>Hello World!</h1>
+    <Header/>
     </>
   )
 }
---
+++ src/Layout/Header.jsx
@@ -0,0 +1,18 @@
+import mealsImage from "../assets/meals.jpg"
+import classes from "./Header.module.css"
+import HeaderCartButton from "./HeaderCartButton"
+
+
+const Header =(props) =>{
+    return <>
+        <header className={classes.header}>
+            <h1>Meals</h1>
+            <HeaderCartButton />
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt="Meals image"/>
+        </div>
+    </>
+}
+
+export default Header;
