7cb630464ef984f0cedeada7524d95c080838a9e
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2023-11-11 06:11:02 +0200

    add the meals component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./Layout/Header"
-
+import Meals from "./Meals/Meals"
 
 const App=()=> {
 
   return (
     <>
     <Header/>
+    <main>
+      <Meals />
+    </main>
     </>
   )
 }
