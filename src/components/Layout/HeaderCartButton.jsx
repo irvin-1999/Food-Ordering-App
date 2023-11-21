bae5a9cd0be17e711486ca4b013fe19ecfeb3c50
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2023-11-21 11:32:52 +0200

    add context for cart components and use reducer hook

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,13 +1,21 @@
 import CartIcon from '../../components/Cart/CartIcon'
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cartContext';
+import { useContext } from 'react';
 
 const HeaderCartButton=props=>{
+    const cartCtx=useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items
+        ? cartCtx.items.reduce((acc, item) => acc + item.amount, 0)
+        : 0;
+
     return <button className={classes.button} onClick={props.onClickk}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>5</span>
+        <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
 }
 
