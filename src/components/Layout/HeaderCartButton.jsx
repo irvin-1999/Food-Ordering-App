c6c9561eff8ad11a97ec298ac504937643807025
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2023-12-01 15:25:40 +0200

    add the initial cart reducer to the Provider

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,7 +1,14 @@
+import { useContext } from 'react';
 import CartIcon from '../../components/Cart/CartIcon'
+import CartContext from '../../store/cartContext';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton=props=>{
+    const cartCtx=useContext(CartContext);
+
+    const numberOfCartItems=cartCtx.items.reduce((acc, item)=>{
+        return acc + item.amount
+    }, 0);
 
     return <button className={classes.button} onClick={props.onClickk}>
         <span className={classes.icon}>
