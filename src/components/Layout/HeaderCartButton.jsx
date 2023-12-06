96045d5725197b901206387fc41fd49ecb663df0
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2023-12-06 13:52:47 +0200

    add useEffect hook in the cart button to add virsual effect when we add an item to the cart

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,16 +1,39 @@
-import { useContext } from 'react';
+import { useContext, useEffect,useState } from 'react';
 import CartIcon from '../../components/Cart/CartIcon'
 import CartContext from '../../store/cartContext';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton=props=>{
+
+    const [btnIsHighLighted,setbtnIsHighLighted] = useState(false)
+
     const cartCtx=useContext(CartContext);
 
-    const numberOfCartItems=cartCtx.items.reduce((acc, item)=>{
+    const {items} = cartCtx
+
+    const numberOfCartItems = items.reduce((acc, item)=>{
         return acc + item.amount
     }, 0);
 
-    return <button className={classes.button} onClick={props.onClickk}>
+
+    const btnClasses = `${classes.button} ${btnIsHighLighted ? classes.bump : ""}`
+
+    useEffect(()=>{
+        if(items.length === 0){
+            return
+        }
+        setbtnIsHighLighted(true)
+
+        const timer = setTimeout(()=>{
+            setbtnIsHighLighted(false)
+        },300)
+
+        return ()=>{
+            clearTimeout(timer)
+        }
+    },[items])
+
+    return <button className={btnClasses} onClick={props.onClickk}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
